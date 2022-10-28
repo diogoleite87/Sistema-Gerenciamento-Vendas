ae87f135d46df60c6450d7ba5b6fb1c5cab43ccc
Diogo Leite <diogoleite87@gmail.com>
2022-10-27 23:09:32 -0300

    first commit

---
+++ backend/prisma/migrations/20221028020539_initial/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "cpf" INTEGER NOT NULL,
+    "email" TEXT NOT NULL,
+    "cell" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "endereco" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("cell")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "name" TEXT NOT NULL,
+    "descrption" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "size" INTEGER NOT NULL,
+    "model" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sold" (
+    "id" SERIAL NOT NULL,
+    "productID" INTEGER NOT NULL,
+    "clientCell" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sold_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "sold" ADD CONSTRAINT "sold_productID_fkey" FOREIGN KEY ("productID") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sold" ADD CONSTRAINT "sold_clientCell_fkey" FOREIGN KEY ("clientCell") REFERENCES "clients"("cell") ON DELETE RESTRICT ON UPDATE CASCADE;
