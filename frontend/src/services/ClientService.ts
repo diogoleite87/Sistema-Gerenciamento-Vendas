8417956ba76b12d6b8ab5ccb473fd388d8470a88
Diogo Leite <diogoleite87@gmail.com>
2022-10-30 21:12:59 -0300

    feat: add get client and update client

--- frontend/src/services/ClientService.ts
+++ frontend/src/services/ClientService.ts
@@ -4,9 +4,13 @@ import { Client, CreateClientDTO } from "../schemas";
 const createClient = (client: CreateClientDTO) => Api.post(`/clients`, client);
 const getAllClient = () => Api.get<Client[]>(`/clients`);
 const deleteClient = (cell: string) => Api.delete(`clients/${cell}`);
+const getClient = (cell: string) => Api.get<Client>(`clients/${cell}`);
+const updateClient = (cell: string, client: CreateClientDTO) => Api.put(`clients/${cell}`, client);
 
 export const ClientService = {
     createClient,
     getAllClient,
-    deleteClient
+    deleteClient,
+    updateClient,
+    getClient
 }
