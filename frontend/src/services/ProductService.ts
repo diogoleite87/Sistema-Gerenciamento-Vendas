19faa580c3fbd0842beb9138fda1ce3e533d5c96
Diogo Leite <diogoleite87@gmail.com>
2022-10-31 19:49:53 -0300

    feat: add get group products avaiable

--- frontend/src/services/ProductService.ts
+++ frontend/src/services/ProductService.ts
@@ -1,14 +1,16 @@
 import { Api } from "../providers";
-import { Product, CreateProductDTO } from "../schemas";
+import { Product, CreateProductDTO, GroupProducts } from "../schemas";
 
 const createProduct = (product: CreateProductDTO) => Api.post(`/products`, product);
 const deleteProduct = (id: number) => Api.delete(`/products/${id}`);
 const getAllProducts = () => Api.get<Product[]>(`/products`);
 const getAllProductsAvaiable = () => Api.get<Product[]>(`/products/avaiable`)
+const getGroupProductsAvaiable = () => Api.get<GroupProducts[]>(`/products/group/avaiable`);
 
 export const ProductService = {
     createProduct,
     deleteProduct,
     getAllProducts,
-    getAllProductsAvaiable
+    getAllProductsAvaiable,
+    getGroupProductsAvaiable
 }
