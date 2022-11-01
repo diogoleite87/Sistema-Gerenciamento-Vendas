5da056a92e85410fa49a8cca7fb76114935fdeeb
Diogo Leite <diogoleite87@gmail.com>
2022-11-01 11:14:49 -0300

    update: change id table

--- backend/prisma/migrations/20221101062411_initial/migration.sql
+++ backend/prisma/migrations/20221101062411_initial/migration.sql
@@ -39,8 +39,28 @@ CREATE TABLE "sold" (
     CONSTRAINT "sold_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "makers" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "makers_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "variations" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "maker_name" TEXT NOT NULL,
+
+    CONSTRAINT "variations_pkey" PRIMARY KEY ("name")
+);
+
 -- AddForeignKey
 ALTER TABLE "sold" ADD CONSTRAINT "sold_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "sold" ADD CONSTRAINT "sold_client_cell_fkey" FOREIGN KEY ("client_cell") REFERENCES "clients"("cell") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "variations" ADD CONSTRAINT "variations_maker_name_fkey" FOREIGN KEY ("maker_name") REFERENCES "makers"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
