aabe3b36e868f989cbbbc018faf83ca6a27fdae6
Diogo Leite <diogoleite87@gmail.com>
2022-11-02 00:22:01 -0300

    feat: add get all products solds request

--- frontend/src/services/ClientService.ts
+++ frontend/src/services/ClientService.ts
@@ -1,16 +1,18 @@
 import { Api } from "../providers";
-import { Client, CreateClientDTO } from "../schemas";
+import { Client, CreateClientDTO, SoldsProductsDAO } from "../schemas";
 
 const createClient = (client: CreateClientDTO) => Api.post(`/clients`, client);
 const getAllClient = () => Api.get<Client[]>(`/clients`);
 const deleteClient = (cell: string) => Api.delete(`clients/${cell}`);
 const getClient = (cell: string) => Api.get<Client>(`clients/${cell}`);
 const updateClient = (cell: string, client: CreateClientDTO) => Api.put(`clients/${cell}`, client);
+const getAllClientProductsSolds = (cell: string) => Api.get<SoldsProductsDAO[]>(`clients/solds/${cell}`);
 
 export const ClientService = {
     createClient,
     getAllClient,
     deleteClient,
     updateClient,
+    getAllClientProductsSolds,
     getClient
 }
