2fa3e48ba126930a45a1110e5f63e04d69a2d2c6
Diogo Leite <diogoleite87@gmail.com>
2022-11-02 00:21:44 -0300

    feat: add interface solds products

--- frontend/src/schemas/Models.ts
+++ frontend/src/schemas/Models.ts
@@ -45,3 +45,16 @@ export type Color = {
     id: number;
     name: string;
 }
+
+export interface SoldsProductsDAO {
+    name_product: string
+    model_product: string
+    color: string
+    size_product: string
+    value_sold: number
+    name_client: string
+    cell_client: string
+    id_sold: number
+    id_product: number
+    created_sold: Date
+}
