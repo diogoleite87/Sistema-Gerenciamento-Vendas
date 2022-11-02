4a49704eccd67fa5b90cd3b2b54344019f303496
Diogo Leite <diogoleite87@gmail.com>
2022-11-01 23:14:49 -0300

    feat: add interface for solds products

---
+++ backend/src/modules/clients/dtos/SoldsProductsDAO.ts
@@ -0,0 +1,10 @@
+export interface SoldsProductsDAO {
+    name_product: string
+    model_product: string
+    color: string
+    size_product: string
+    value_sold: number
+    name_client: string
+    id_sold: number
+    id_product: number
+}
