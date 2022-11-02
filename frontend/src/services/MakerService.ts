a9fecb31c9adc3132c8b1f737ccda706409a14e7
Diogo Leite <diogoleite87@gmail.com>
2022-11-02 15:17:54 -0300

    feat: add create maker

--- frontend/src/services/MakerService.ts
+++ frontend/src/services/MakerService.ts
@@ -2,9 +2,10 @@ import { Api } from "../providers";
 import { Maker } from "../schemas";
 
 const getAllMakers = () => Api.get<Maker[]>('/makers');
+const createMaker = (name: string) => Api.post(`/makers/${name}`);
 
 export const MakerService = {
 
-    getAllMakers
-
+    getAllMakers,
+    createMaker
 }
