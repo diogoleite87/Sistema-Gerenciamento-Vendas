55af9d670f792ef08af8cc4fbf196f45030a0f1b
Diogo Leite <diogoleite87@gmail.com>
2022-11-02 18:41:21 -0300

    feat: add delete variation

--- frontend/src/services/VariationService.ts
+++ frontend/src/services/VariationService.ts
@@ -4,10 +4,12 @@ import { Variation } from "../schemas";
 const getAllVariation = () => Api.get<Variation[]>(`/variations`);
 const getVariation = (nameMaker: string) => Api.get<Variation[]>(`/variations/${nameMaker}`);
 const createVariation = (nameMaker: string, nameVariation: string) => Api.post(`/variations/${nameMaker}/${nameVariation}`);
+const deleteVariation = (nameVariation: string) => Api.delete(`/variations/${nameVariation}`);
 
 export const VariationService = {
 
     getAllVariation,
     getVariation,
-    createVariation
+    createVariation,
+    deleteVariation
 }
