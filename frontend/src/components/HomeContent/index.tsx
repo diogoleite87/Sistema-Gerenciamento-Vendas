7e6b98243948fe882dad550ccaa2dedc1d63d10d
Diogo Leite <diogoleite87@gmail.com>
2022-11-03 13:51:58 -0300

    feat: add graphics in home page

--- frontend/src/components/HomeContent/index.tsx
+++ frontend/src/components/HomeContent/index.tsx
@@ -1,6 +1,21 @@
+import { Typography } from "@material-ui/core";
+import { Box, Stack } from "@mui/material";
+import GraphicSoldsOfWeek from "../GraphicSoldsOfWeek";
+import GraphicValueSoldsOfWeek from "../GraphicValueSoldsOfWeek";
+
 export default function HomeContent() {
 
     return (
-        <h1>Home Page</h1>
+        <Stack direction='row' spacing={2} width='100%' marginTop='4vh'>
+            <Box sx={{ width: '50%' }}>
+                <Typography variant="h5">Total de Vendas</Typography>
+                <GraphicSoldsOfWeek />
+            </Box>
+
+            <Box sx={{ width: '50%' }}>
+                <Typography variant="h5">Total em R$</Typography>
+                <GraphicValueSoldsOfWeek />
+            </Box>
+        </Stack >
     )
 }
