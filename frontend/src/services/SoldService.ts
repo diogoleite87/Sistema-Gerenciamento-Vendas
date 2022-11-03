2969dc8811daccdccdb0adefb7a169c5fb9d46cc
Diogo Leite <diogoleite87@gmail.com>
2022-11-03 13:52:35 -0300

    feat: add get solds of week request

--- frontend/src/services/SoldService.ts
+++ frontend/src/services/SoldService.ts
@@ -1,12 +1,14 @@
 import { Api } from "../providers";
-import { Product, CreateProductSoldDTO } from "../schemas";
+import { Product, CreateProductSoldDTO, SoldsOfWeek } from "../schemas";
 
 const createProductSold = (productSold: CreateProductSoldDTO) => Api.post(`/solds`, productSold);
 const deleteProductSold = (id: number, cell: string) => Api.delete(`/solds/${id}/${cell}`);
 const getAllProductSold = () => Api.get<Product[]>(`/solds`);
+const getSoldsOfWeek = (date: string, count: number) => Api.get<SoldsOfWeek[]>(`/solds/week/${date}/${count}`)
 
 export const SoldService = {
     createProductSold,
     deleteProductSold,
-    getAllProductSold
+    getAllProductSold,
+    getSoldsOfWeek
 }
