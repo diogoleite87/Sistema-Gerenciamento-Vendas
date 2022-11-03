71434bb0dccfbb2e030261e13ec3fab320516b15
Diogo Leite <diogoleite87@gmail.com>
2022-11-03 00:06:00 -0300

    fix: fix return days of week

--- backend/src/modules/products/useCases/getSoldsOfWeek/GetSoldsOfWeekUseCase.ts
+++ backend/src/modules/products/useCases/getSoldsOfWeek/GetSoldsOfWeekUseCase.ts
@@ -10,7 +10,7 @@ export class GetSoldsOfWeekUseCase {
 
         for (let i = 0; i < count; i++) {
 
-            let dayOfWeek = moment(dateRequest).add(- count + i, 'days')
+            let dayOfWeek = moment(dateRequest).add(- (count - 1) + i, 'days')
             let sql = `SELECT SUM(s.value_sold) AS sumsolds, COUNT(*) AS countsolds FROM sold s WHERE CAST (s.created_at AS Text) LIKE '${dayOfWeek.format('YYYY-MM-DD')}%'`
 
             let jsonDayOfWeek = await prisma.$queryRawUnsafe(sql);
