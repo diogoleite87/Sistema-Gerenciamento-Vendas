067391af2b01e9854a7604c28ae91cf4037b0ca9
Diogo Leite <diogoleite87@gmail.com>
2022-11-05 15:54:37 -0300

    feat: add get info solds request

--- frontend/src/services/SoldService.ts
+++ frontend/src/services/SoldService.ts
@@ -1,14 +1,16 @@
 import { Api } from "../providers";
-import { Product, CreateProductSoldDTO, SoldsOfWeek } from "../schemas";
+import { Product, CreateProductSoldDTO, SoldsOfWeek, InfoSolds } from "../schemas";
 
 const createProductSold = (productSold: CreateProductSoldDTO) => Api.post(`/solds`, productSold);
 const deleteProductSold = (id: number, cell: string) => Api.delete(`/solds/${id}/${cell}`);
 const getAllProductSold = () => Api.get<Product[]>(`/solds`);
 const getSoldsOfWeek = (date: string, count: number) => Api.get<SoldsOfWeek[]>(`/solds/week/${date}/${count}`)
+const getInfoSolds = () => Api.get<InfoSolds>('/solds/info');
 
 export const SoldService = {
     createProductSold,
     deleteProductSold,
     getAllProductSold,
-    getSoldsOfWeek
+    getSoldsOfWeek,
+    getInfoSolds
 }
