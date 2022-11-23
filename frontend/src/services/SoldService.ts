f666982bbc4ac4958e093a4e328d388558d66c7d
Diogo Leite <diogoleite87@gmail.com>
2022-11-22 22:55:04 -0300

    feat: add get products by date service

--- frontend/src/services/SoldService.ts
+++ frontend/src/services/SoldService.ts
@@ -1,16 +1,18 @@
 import { Api } from "../providers";
-import { Product, CreateProductSoldDTO, SoldsOfWeek, InfoSolds } from "../schemas";
+import { Product, CreateProductSoldDTO, SoldsOfWeek, InfoSolds, ProductSoldByDate } from "../schemas";
 
 const createProductSold = (productSold: CreateProductSoldDTO) => Api.post(`/solds`, productSold);
 const deleteProductSold = (id: number, cell: string) => Api.delete(`/solds/${id}/${cell}`);
 const getAllProductSold = () => Api.get<Product[]>(`/solds`);
 const getSoldsOfWeek = (date: string, count: number) => Api.get<SoldsOfWeek[]>(`/solds/week/${date}/${count}`)
 const getInfoSolds = () => Api.get<InfoSolds>('/solds/info');
+const getProductsSoldsByDate = (date: string) => Api.get<ProductSoldByDate[]>(`/solds/date/${date}`)
 
 export const SoldService = {
     createProductSold,
     deleteProductSold,
     getAllProductSold,
     getSoldsOfWeek,
-    getInfoSolds
+    getInfoSolds,
+    getProductsSoldsByDate
 }
