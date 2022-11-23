7c04f66de664a45730d46e22aab4d497bff989b4
Diogo Leite <diogoleite87@gmail.com>
2022-11-22 22:25:23 -0300

    fix: product value toFixed float

--- frontend/src/components/TableProducts/index.tsx
+++ frontend/src/components/TableProducts/index.tsx
@@ -167,7 +167,7 @@ export default function TableProducts({ type }: ITableProducts) {
                                             {product.color}
                                         </TableCell>
                                         <TableCell align='center'>
-                                            R${product.value}
+                                            R${product.value.toFixed(2)}
                                         </TableCell>
                                         <TableCell>
                                             {product.description}
